bac9e980471a3c1dfac0581c70c72f4b3ab7dfb0
roberta2902 <roberta.cillia@studenti.unipd.it>
2024-06-27 23:24:20 +0200

    Piccole modifche alla pagina welcome,profile e questionario

--- project/lib/screens/welcome.dart
+++ project/lib/screens/welcome.dart
@@ -1,16 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:project/screens/questionnaire.dart'; // Assicurati di importare la pagina del questionario
+import 'package:project/screens/questionnaire.dart'; 
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: PageView(
+        child: Stack(
           children: [
-            _buildWelcomeImage('lib/assets/welcome1.png'),
-            _buildWelcomeImage('lib/assets/welcome2.png'),
-            _buildWelcomeImageWithStartButton('lib/assets/welcome3.png', context),
+            PageView(
+              controller: _pageController,
+              onPageChanged: (int page) {
+                setState(() {
+                  _currentPage = page;
+                });
+              },
+              children: [
+                _buildWelcomeImage('lib/assets/welcome1.png'),
+                _buildWelcomeImage('lib/assets/welcome2.png'),
+                _buildWelcomeImageWithStartButton('lib/assets/welcome3.png', context),
+              ],
+            ),
+            Positioned(
+              bottom: 20,
+              left: 20,
+              child: Visibility(
+                visible: _currentPage > 0,
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    if (_currentPage > 0) {
+                      _pageController.previousPage(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 20,
+              right: 20,
+              child: Visibility(
+                visible: _currentPage < 2,
+                child: IconButton(
+                  icon: Icon(Icons.arrow_forward),
+                  onPressed: () {
+                    if (_currentPage < 2) {
+                      _pageController.nextPage(
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.easeInOut,
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),
