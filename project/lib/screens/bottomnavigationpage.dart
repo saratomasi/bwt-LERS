96af47730dfe387cc6d50575d108d3949e57317e
roberta2902 <roberta.cillia@studenti.unipd.it>
2024-07-06 22:35:09 +0200

    Creazione pagina ManualSearchPage per la ricerca manuale, piccola modifica ad alcune immagini e modifica alla bottomnavigationpage

--- project/lib/screens/bottomnavigationpage.dart
+++ project/lib/screens/bottomnavigationpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project/screens/autosearchPage.dart';
+import 'package:project/screens/ManualSearchPage.dart'; 
 import 'package:project/screens/profile.dart';
 import 'package:project/screens/homepage.dart';
 import 'package:project/widgets/expandableFab.dart';
@@ -39,66 +40,48 @@ class _HomePageState extends State<BottomNavigationBarPage> {
     }
   }
 
-//x button che si espande:
-  static const _actionTitles = ['AI advice!', 'Search page'];
-
-  void _showAction(BuildContext context, int index) {
-    showDialog<void>(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          content: Text(_actionTitles[index]),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('CLOSE'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _showPage(index: _showIndex),
       bottomNavigationBar: BottomNavigationBar(
-          onTap: (index) {
-            setState(() {
-              _showIndex = index;
-            });
-          },
-          currentIndex: _showIndex,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
-
-            //aggiungere bottone nuova sessione
-          ]),
+        onTap: (index) {
+          setState(() {
+            _showIndex = index;
+          });
+        },
+        currentIndex: _showIndex,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+        ],
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: ExpandableFab(
         distance: 80,
         children: [
-          //ActionButton(onPressed: () => _showAction(context, 0), icon: const Icon(Icons.auto_awesome),text: 'Get advice'),
           ActionButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => AutoSearch()));
-              },
-              icon: const Icon(Icons.auto_awesome),
-              text: 'Get advice'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AutoSearch()),
+              );
+            },
+            icon: const Icon(Icons.auto_awesome),
+            text: 'Get advice',
+          ),
           ActionButton(
-              onPressed: () => _showAction(context, 1),
-              icon: const Icon(Icons.travel_explore),
-              text: 'Manual search'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ManualSearch()), 
+              );
+            },
+            icon: const Icon(Icons.travel_explore),
+            text: 'Manual search',
+          ),
         ],
       ),
-      //floatingActionButton: FloatingActionButton(
-      //onPressed: _incrementCounter,
-      //tooltip: 'Increment',
-      //child: const Icon(Icons.add),
-      //),
     );
   }
 }
